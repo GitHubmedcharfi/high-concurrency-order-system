4805ceeeda70bf71183df68e2731ab93b512ab27
GitHubmedcharfi <mohamedcharfi4070@gmail.com>
2026-05-30 11:56:26 +0100

    feat: add Dockerfile, docker-compose and dynamic application.properties

---
+++ high-concurrency-order-system/src/main/java/tn/high_concurrency_order/system/HighConcurrencyOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package tn.high_concurrency_order.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class HighConcurrencyOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrencyOrderSystemApplication.class, args);
+	}
+
+}
---
+++ high-concurrency-order-system/src/test/java/tn/high_concurrency_order/system/HighConcurrencyOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.high_concurrency_order.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
